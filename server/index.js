464559a9ac1d9beda03eedba79f231fbcf74d8c3
OjalSingh <ojals007@gmail.com>
2026-01-11 13:10:24 +0530

    Login-Register verified

--- server/index.js
+++ server/index.js
@@ -1,41 +1,22 @@
-const healthRoutes = require("./routes/health");
-
-
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 
-//importing
 const authRoutes = require("./routes/auth");
-const productRoutes = require("./routes/products");
-const orderRoutes = require("./routes/orders");
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
-app.use("/api", healthRoutes);
 
-
-// Routes
 app.use("/api/auth", authRoutes);
-app.use("/api/products", productRoutes);
-app.use("/api/orders", orderRoutes);
 
-// Test route (optional, to check server is running)
-app.get("/", (req, res) => {
-  res.send("API is running 🚀");
-});
-
-// MongoDB connection
-const PORT = process.env.PORT || 5000;
-
-mongoose
-  .connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI)
   .then(() => {
-    console.log("✅ MongoDB connected");
-    app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+    console.log("MongoDB connected");
+    app.listen(5000, () =>
+      console.log("Server running on port 5000")
+    );
   })
-  .catch(err => console.error("❌ MongoDB connection error:", err));
+  .catch(err => console.error(err));
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -31,4 +31,35 @@ router.post("/register", async (req, res) => {
   }
 });
 
+// LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ message: "All fields required" });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    if (user.password !== password) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    res.status(200).json({
+      message: "Login successful",
+      user: {
+        id: user._id,
+        email: user.email
+      }
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+
 module.exports = router;
